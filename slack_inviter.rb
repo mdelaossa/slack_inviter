6e50f64877bfcb7c2ed7b8b8b7bd662fc477dd66
Mario de la Ossa <mariodelaossa@gmail.com>
2015-05-08 16:52:17 -0600

    Added the team name to view

--- slack_inviter.rb
+++ slack_inviter.rb
@@ -1,13 +1,16 @@
 require 'sinatra'
 require 'json'
+require 'yaml'
 require './api-caller'
 require './slack-helper'
 
 set :bind, "0.0.0.0"
 
 slack = SlackHelper.new
+config = YAML::load_file 'config.yml'
 
 get '/' do
+  @team_name = config[:team]
   erb :index
 end
 
