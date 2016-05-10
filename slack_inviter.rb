a23f5b1d068eff22acd4b18d5c5b6c179465960d
Mario de la Ossa <mariodelaossa@gmail.com>
2016-05-10 09:30:47 -0600

    Modified to run on newer Sinatra

--- slack_inviter.rb
+++ slack_inviter.rb
@@ -4,31 +4,34 @@ require 'yaml'
 require './api-caller'
 require './slack-helper'
 
-set :bind, "0.0.0.0"
+class Sinatra::Base
 
-config = YAML::load_file 'config.yml'
-slack = SlackHelper.new config
+  set :bind, "0.0.0.0"
 
-get '/' do
-  @team_name = config[:team]
-  @passphrase = true if config[:passphrase]
-  erb :index
-end
+  config = YAML::load_file 'config.yml'
+  slack = SlackHelper.new config
 
-post '/get_invite' do
-  if params['passphrase'] == config[:passphrase]
-    @user = OpenStruct.new(params['user'])
-    response = slack.invite(@user.email, @user.name)
-    logger.info response
-    result = JSON.parse response.body
-    logger.info result
-
-    @success = result['ok']
-    @error = result['error']
-  else
-    @success = false
-    @error = 'Wrong passphrase'
+  get '/' do
+    @team_name = config[:team]
+    @passphrase = true if config[:passphrase]
+    erb :index
   end
 
-  erb :get_invite
+  post '/get_invite' do
+    if params['passphrase'] == config[:passphrase]
+      @user = OpenStruct.new(params['user'])
+      response = slack.invite(@user.email, @user.name)
+      logger.info response
+      result = JSON.parse response.body
+      logger.info result
+
+      @success = result['ok']
+      @error = result['error']
+    else
+      @success = false
+      @error = 'Wrong passphrase'
+    end
+
+    erb :get_invite
+  end
 end
