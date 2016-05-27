7d2ecb93a224531c9eed65f66372d2c33d86741f
Mario de la Ossa <mariodelaossa@gmail.com>
2016-05-27 12:10:49 -0600

    Added the ability to invite users as guests/restricted users

--- slack-helper.rb
+++ slack-helper.rb
@@ -7,13 +7,16 @@ class SlackHelper
     team_name = config[:team]
     api_token = config[:api_key]
     @channels = config[:channels].join(', ')
+    @restricted = config[:restricted] || false
     @api = ApiCaller.new uri: URI("https://#{team_name}.slack.com"), base_path: "/api", post_params: { token: api_token }
     auth_test = @api.post "auth.test"
     puts "Auth text: #{JSON.parse(auth_test.body)}"
   end
 
   def invite(email, name)
-    @api.post "users.admin.invite?t=${Time.now.to_i}", { email: email, first_name: name, channels: @channels, set_active: true, _attempts: 1 }
+    params = { email: email, first_name: name, channels: @channels, set_active: true, _attempts: 1 }
+    params[:ultra_restricted] = true if @restricted
+    @api.post "users.admin.invite?t=${Time.now.to_i}", params
   end
 
 end
